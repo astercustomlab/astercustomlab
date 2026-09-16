1b3d9b81fa17d1766164d16f0c94f17842fc6659
mshlir <heri21440@gmail.com>
2026-09-16 17:00:36 +0700

    update google 2

--- app/layout.js
+++ app/layout.js
@@ -69,7 +69,13 @@ export const metadata = {
     images: ["/logo.png"],
   },
   icons: {
-    icon: "/logo.png",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-48.png", type: "image/png", sizes: "48x48" },
+      { url: "/icon-512.png", type: "image/png", sizes: "512x512" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
   verification: {
     google: "8HA23TGOjjOJL0W90Hve3zzYvX_xsumphrXcmLpk-yU",
